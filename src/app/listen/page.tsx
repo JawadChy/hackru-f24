08cb245952e457557d5ee1d1fc7197df34715f75
Jerrely Vizueta <94874872+Jerrely1618@users.noreply.github.com>
2024-10-26 14:25:19 -0400

    Updated Camera

--- src/app/listen/page.tsx
+++ src/app/listen/page.tsx
@@ -1,8 +1,15 @@
+'use client';
 import CameraBucket from "@/components/ui/camera-bucket"
+import { useEffect, useState } from "react";
 export default function Listen(){
+    const [image, setImage] = useState<string | null>(null);
+    useEffect(() => {
+        console.log(image);
+    }, [image])
+
     return(
         <div>
-        <CameraBucket/>
+            <CameraBucket setImage={setImage}/>
         </div>
     )
 }
--- src/components/ui/camera-bucket.tsx
+++ src/components/ui/camera-bucket.tsx
@@ -1,35 +1,39 @@
 'use client';
 
 import { useRef } from "react";
-import { Camera } from "react-camera-pro";
-export default function CameraBucket(){
-    const cam = useRef(null);
+import { Camera, CameraType } from "react-camera-pro";
+export default function CameraBucket({setImage: setImage}: {setImage: (image: string) => void}){
+    const camera = useRef<CameraType>(null);
+
+    const takePicture = () => {
+        if (camera.current) {
+            const image = camera.current.takePhoto();
+            setImage(image as string);
+        }
+    };
 
     return(
-        <>
-        <div className="relative flex min-h-screen w-full flex-col items-center justify-center overflow-hidden bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-            <div className="relative z-10 flex w-full h-[200px]  min-h-screen max-w-7xl flex-col items-center justify-center px-4">
-                <div className="animate-fade-in h-[200px] ">
-                    <div className=" h-[200px] bg-gradient-to-b from-purple-50 to-purple-400 bg-clip-text py-4 text-center text-4xl font-bold text-transparent md:text-7xl">
-
-                    <Camera
-                        aspectRatio="cover"
-                        facingMode="environment"
-                        errorMessages={{
-                            noCameraAccessible: 'No camera device accessible. Please connect your camera or try a different browser.',
-                            permissionDenied: 'Permission denied. Please refresh and give camera permission.',
-                            switchCamera:
-                            'It is not possible to switch camera to different one because there is only one video device accessible.',
-                            canvas: 'Canvas is not supported.',
-                        }}
-                        videoReadyCallback={() => {
-                            console.log('Video feed ready.');
-                        }}
-                    />
-                    </div>
-                </div>
-            </div>  
-            </div>    
-        </>
+            <button onClick={takePicture} >
+                <div className={`transition-all duration-300 absolute top-10 right-10 z-20 rounded-2xl`}>
+                    <div className={`transition-all duration-300 rounded-xl w-[200px] h-[113px] hover:w-[300px] hover:h-[169px]`}>
+                        <Camera
+                            ref={camera}
+                            aspectRatio="cover"
+                            facingMode="environment"
+                            errorMessages={{
+                                noCameraAccessible: 'No camera device accessible. Please connect your camera or try a different browser.',
+                                permissionDenied: 'Permission denied. Please refresh and give camera permission.',
+                                switchCamera:
+                                'It is not possible to switch camera to different one because there is only one video device accessible.',
+                                    canvas: 'Canvas is not supported.',
+                            }}
+                            videoReadyCallback={() => {
+                                console.log('Video feed ready.');
+                            }}
+                        />
+                        <div className="h-full bg-red-200 z-30"/>
+                    </div>  
+                </div>  
+            </button>
     )
 }
