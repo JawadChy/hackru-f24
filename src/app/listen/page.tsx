23e40fbef4c0899b563ad3ee58668dc092c542c1
Jawad Chowdhury <124181330+JawadChy@users.noreply.github.com>
2024-10-26 22:55:15 -0400

    fix merge issue

--- src/app/listen/page.tsx
+++ src/app/listen/page.tsx
@@ -87,25 +87,6 @@ export default function Listen() {
     return(
         <div className="flex flex-col w-full justify-center items-center p-5">
             <CameraBucket setImage={setImage}/>
-            <div className="absolute top-32 right-72 z-20 flex flex-col -rotate-[32deg] items-center">
-                <ArrowTrendingUpIcon className="text-white h-5 w-5 w-full transform -rotate-[80deg]" />
-                <span className="text-center text-sm toon m-0 w-full leading-tight text-white">
-                    Take pic!
-                </span>
-            </div>
-            {isAnalyzing && (
-                <div className="mt-4 text-white">Analyzing emotions...</div>
-            )}
-            {emotionData && emotionData.faces.length > 0 && (
-                <div className="mt-4 text-white">
-                    <h3>Detected Emotions:</h3>
-                    {emotionData.faces.map((face, index) => (
-                        <div key={index} className="mt-2">
-                            <p>Face {index + 1} - Dominant Emotion: {face.dominant_emotion}</p>
-                        </div>
-                    ))}
-                </div>
-            )}
             <HoverEffect items={mockData} />
         </div>
     )
