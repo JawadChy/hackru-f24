58a1d2f3858ea8c0742e1497ee890126a439b706
Jawad Chowdhury <124181330+JawadChy@users.noreply.github.com>
2024-10-26 23:09:57 -0400

    background fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,10 +39,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`bg-black bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] ${geistSans.variable} ${geistMono.variable}  ${merriweather.className} antialiased`}
+        className={` ${geistSans.variable} ${geistMono.variable}  ${merriweather.className} antialiased`}
       >
           {children}
-          
+          <div className="h-screen bg-black absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       </body>
     </html>
   );
